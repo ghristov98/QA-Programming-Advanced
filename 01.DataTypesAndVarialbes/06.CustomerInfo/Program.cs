ccff884cfd619ad99c81885f5ebed770757a82c0
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 12:19:38 +0300

    Customer Info
    Write a program that:
    
    · Reads customer's first name, last name and email
    
    · Prints the read data in the following format:
    
    "Customer: {first name} {last name} ({email})"

---
+++ 01.DataTypesAndVarialbes/06.CustomerInfo/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string firstName = Console.ReadLine();
+
+string lastName = Console.ReadLine();
+
+string email = Console.ReadLine();
+
+Console.WriteLine($"Customer: {firstName} {lastName} ({email})");
