fb93c7c668ca607cfffc933c2043003a0dc379da
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:08:48 +0300

    Deposit Calculator
    Write a program that calculates how much you will receive at the end of the deposit period at a certain interest rate. Use the following formula:
    
    amount = deposited amount + term of deposit * (deposited amount * annual interest rate) / 12

---
+++ 02.KeyConcepts/05.DepositCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// Input
+double depositAmount = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine());
+double yearinterestRate = double.Parse(Console.ReadLine());
+
+// Calculations
+
+double yearIncome = depositAmount * (yearinterestRate / 100);
+double monthIncome = yearIncome / 12;
+
+double totalIncome = depositAmount + (months * monthIncome);
+
+// Output
+
+Console.WriteLine(totalIncome);
