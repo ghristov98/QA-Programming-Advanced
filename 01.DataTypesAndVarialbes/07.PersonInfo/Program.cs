b9849ee70963c0b158a59519536d5165826107eb
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 12:20:46 +0300

    Person Info
    Write a program to read and print info about a person:
    
    · Reads 4 lines of input: first name, last name, country and town
    
    · Prints information about a person in the following format:
    
    "{first name} {last name} from {country} – {town}!"

---
+++ 01.DataTypesAndVarialbes/07.PersonInfo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string firstName = Console.ReadLine();
+
+string lastName = Console.ReadLine();
+
+string country = Console.ReadLine();
+
+string town = Console.ReadLine();
+
+Console.WriteLine($"{ firstName } { lastName } from { country } - { town }!");
