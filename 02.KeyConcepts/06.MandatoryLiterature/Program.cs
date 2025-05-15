f7ab5682d978218002821e695e94a8b8b92eb800
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:09:59 +0300

    Mandatory Literature
    For the summer holidays, there are a certain number of books on Joro's list of mandatory literature. Since Joro prefers to play with friends outside, your task is to help him calculate how many hours a day he should devote to reading the necessary literature

---
+++ 02.KeyConcepts/06.MandatoryLiterature/Program.cs
@@ -0,0 +1,14 @@
+﻿// Input
+int pagesNumber = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());   
+int days = int.Parse(Console.ReadLine());
+
+// Calculations
+
+int totalTime = pagesNumber / pagesPerHour;
+
+int hoursPerDay = totalTime / days;
+
+// Output
+
+Console.WriteLine(hoursPerDay);
