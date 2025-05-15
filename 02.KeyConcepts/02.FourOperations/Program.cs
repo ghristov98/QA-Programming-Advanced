f2dd8073430f5c24fef81ad53de4327611eccbed
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:03:03 +0300

    Four Operations
    Write a program that:
    
    · Read two floating-point numbers: first number and second number
    
    · Performs 4 arithmetic operations on the given 2 numbers, in the following order:
    
    o Addition (+)
    
    o Subtraction (-)
    
    o Multiplication (*)
    
    o Division (/)
    
    · Print the results, all formatted to the 2nd digit, in the following format:
    
    o "{first number} + {second number} = {addition result}"
    
    o "{first number} - {second number} = {subtraction result}"
    
    o "{first number} * {second number} = {multiplication result}"
    
    o "{first number} / {second number} = {division result}"

---
+++ 02.KeyConcepts/02.FourOperations/Program.cs
@@ -0,0 +1,31 @@
+﻿// Input
+
+double firstNumber = double.Parse(Console.ReadLine());
+
+double secondNumber = double.Parse(Console.ReadLine());
+
+// Calculations
+
+// Addition
+
+double sum = firstNumber + secondNumber;
+
+// Subtraction
+
+double subs = firstNumber - secondNumber;
+
+// Multiplication
+
+double mult = firstNumber * secondNumber;
+
+// Division
+
+double div = firstNumber / secondNumber;
+
+// Output
+
+Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {sum:F2}");
+Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {subs:F2}");
+Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {mult:F2}");
+Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {div:F2}");
+
