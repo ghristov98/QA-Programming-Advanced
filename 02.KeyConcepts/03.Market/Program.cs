fe8d184eb18ebd45226f01e05b50b1860420f3f7
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:04:32 +0300

    Market
    Write a program that:
    
    · You have a farmer sells tomatoes and cucumbers at the market
    
    · Read four floating-point numbers:
    
    o First represents tomato price
    
    o Second represents tomato quantity
    
    o Third represents cucumber price
    
    o Forth represents cucumber quantity
    
    · Calculate the total cost of the production by given quantities and prices
    
    · Print the total cost, formatted to the 2nd digit

---
+++ 02.KeyConcepts/03.Market/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+double tomatoesPrice = double.Parse(Console.ReadLine());
+double tomatoesQuantity = double.Parse(Console.ReadLine());
+double cucumberPrice = double.Parse(Console.ReadLine());
+double cucumberQuantity = double.Parse(Console.ReadLine());
+
+// Calculations
+
+double tomatoesTotal = tomatoesPrice * tomatoesQuantity;   
+double cucumberTotal = cucumberPrice * cucumberQuantity;
+double totalPrice = tomatoesTotal + cucumberTotal;
+
+// Output
+
+Console.WriteLine($"{totalPrice:F2}"); 
