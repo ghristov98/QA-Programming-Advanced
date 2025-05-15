afaeaa67c4c7ca6a22215b58045f5dac70b4c9c7
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:05:46 +0300

    Tiles
    Write a program that:
    
    · You have a rectangular bathroom of size W x H
    
    · We want to cover it with tiles of size Wt x Ht
    
    · Read four floating-point numbers:
    
    o First represents bathroom width (W)
    
    o Second represents bathroom height (H)
    
    o Third represents tile width (Wt)
    
    o Forth represents tile height (Ht)
    
    · Calculate how many tiles will be needed (add 10% surplus)
    
    · Print the count of the needed tiles, rounded to the nearest intege

---
+++ 02.KeyConcepts/04.Tiles/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeight = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeight = double.Parse(Console.ReadLine());
+
+// Calculations
+double bathroomArea = bathroomWidth * bathroomHeight;
+double bathroomAreaWithSurplus = bathroomArea + (bathroomArea * 0.10);
+
+double tileArea = tileWidth * tileHeight;
+
+double neededTiles = bathroomAreaWithSurplus / tileArea;
+
+// Output
+Console.WriteLine(Math.Round(neededTiles)); // rounding to nearest integer value 
