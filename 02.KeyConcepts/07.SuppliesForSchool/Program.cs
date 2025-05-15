a7c4eb6de72c4be46b87b02a136e6e88d08fa68a
Georgi Hristov <ghristov5208@gmail.com>
2025-05-15 13:10:53 +0300

    Supplies for School
    The school year has already started and the 10B grade manager - Annie has to buy a certain number of packets of pens, packets with markers, as well as board cleaner. She is a regular client of a bookstore, so there is a discount for her, which represents some discount percentage of the total amount. Write a program that calculates how much money Annie will need to collect to pay the bill, keeping in mind the following price list:
    
    · Package of pens - 5.80 lv.
    
    · Package of markers - 7.20 lv.
    
    · Board cleaner - 1.20 BGN (per liter)

---
+++ 02.KeyConcepts/07.SuppliesForSchool/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Prices
+
+double penPrice = 5.80;
+double markerPrice = 7.20;
+double boardCleaner = 1.20;
+
+// Input
+
+int penQuantity = int.Parse(Console.ReadLine());
+int markerQuantity = int.Parse(Console.ReadLine());
+int boardCleanerQuantity = int.Parse(Console.ReadLine());
+int discountPercentage = int.Parse(Console.ReadLine());
+
+// Calculations
+
+double penTotal = penQuantity * penPrice;
+double markerTotal = markerQuantity * markerPrice;
+double boardCleanerTotal = boardCleanerQuantity * boardCleaner;
+double totalPrice = penTotal + markerTotal + boardCleanerTotal;
+double discount = totalPrice * (discountPercentage / 100.0);
+double totalPriceWithDiscount = totalPrice - discount;
+
+// Output
+
+Console.WriteLine($"{totalPriceWithDiscount:F3}");
