d7010e240fc3824f7d32240bed93b096e1acbd1a
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 09:44:27 +0300

    Senturies to Minutes
    Write a program that:
    
    · Reads an integer number, that represents centuries for conversion
    
    · Convert centuries to years (1 century = 100 years)
    
    · Convert years to days (1 year = 365.2422 days)
    
    · Convert days to hours (1 day = 24 hours)
    
    · Convert hours to minutes (1 hour = 60 minutes)
    
    · Print converted data in the following format:

---
+++ 03.DataTypesVariablesAndKeyConcepts/02.CenturiesToMinutes/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Input
+
+int centuries = int.Parse(Console.ReadLine());
+int years = centuries * 100; // 1 centurie = 100 years
+int days = (int)(years * 365.2422); // 1 year = 365.2422 days
+int hours = days * 24;
+int minutes = hours * 60;
+
+// Output
+
+Console.WriteLine($"{ centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
