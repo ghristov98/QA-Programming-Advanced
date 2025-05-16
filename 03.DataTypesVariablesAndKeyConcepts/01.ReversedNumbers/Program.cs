f2135e94b70127f0c4451a2e5ab752d101e44f20
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 09:43:30 +0300

    Reversed Numbers
    Write a program that:
    
    · Reads three floating-point numbers from the console
    
    · Print them in reversed order, each number on a new line

---
+++ 03.DataTypesVariablesAndKeyConcepts/01.ReversedNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Input
+
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+double thirdNumber = double.Parse(Console.ReadLine());
+
+// Output
+
+Console.WriteLine(thirdNumber);
+Console.WriteLine(secondNumber);
+Console.WriteLine(firstNumber);
