cf8d4f0c411de15db7d0e0d47c718e13ee5949af
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 10:04:08 +0300

    Food Delivery
    Input
    
    From the console read 3 lines:
    
    · Number of chicken menus – integer in the range [0 ... 99]
    
    · Number of menus with fish – integer in the range [0 ... 99]
    
    · Number of vegetarian menus – an integer in the range [0 ... 99]
    
    Output
    
    Print out only one line on the console: "{order price}"

---
+++ 03.DataTypesVariablesAndKeyConcepts/04.FoodDelivery/Program.cs
@@ -0,0 +1,24 @@
+﻿//• Chicken menu – 10.35 lv.
+
+//• Menu with fish – 12.40 lv.
+
+//• Vegetarian menu – 8.15 lv. 
+
+
+//Input
+int chickenMenu = int.Parse(Console.ReadLine());
+int fishMenu = int.Parse(Console.ReadLine());
+int vegetarianMenu = int.Parse(Console.ReadLine());
+
+//Calculations
+double priceChickenMenu = chickenMenu * 10.35;
+double priceFishMenu = fishMenu * 12.40;
+double priceVegetarianMenu = vegetarianMenu * 8.15;
+double totalPriceMenus = priceChickenMenu + priceFishMenu + priceVegetarianMenu;
+double priceDessert = totalPriceMenus * 0.2;
+double priceDelivery = 2.50;//conditional
+double totalPrice = totalPriceMenus + priceDessert + priceDelivery;
+
+//Output
+
+Console.WriteLine(totalPrice);
