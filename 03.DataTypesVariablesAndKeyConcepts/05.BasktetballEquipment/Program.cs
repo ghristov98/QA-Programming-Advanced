6c08812149ca15d9beac21fa3df4bdada61673e0
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 10:05:44 +0300

    Basketball equipment
    Write a program that calculates the expenses of Jesse if he starts training, knowing how much is the fee for basketball training for a period of 1 year.
    
    · Basketball sneakers – their price is 40% less than the fee for one year
    
    · Basketball uniform – its price is 20% cheaper than that of sneakers
    
    · Basketball ball – its price is 1 / 4 of the price of the basketball uniform
    
    · Basketball accessories – their price is 1 / 5 of the price of the basketball ball
    
    Input
    
    From the console read 1 row:
    
    · The annual basketball training fee – an integer in the range [0... 9999]
    
    Output
    
    Print on the console how much Jesse will spend if he starts playing basketball.

---
+++ 03.DataTypesVariablesAndKeyConcepts/05.BasktetballEquipment/Program.cs
@@ -0,0 +1,15 @@
+﻿// Input
+int trainingFee = int.Parse(Console.ReadLine());
+
+//Calculations
+
+int pricePerYear = trainingFee;
+int priceSneakers = (int)(pricePerYear - (pricePerYear * 0.4));
+double priceUniform = priceSneakers - (priceSneakers * 0.2);
+double priceBall = priceUniform * 0.25;
+double priceAccessories = priceBall * 0.2;
+double priceTotal = pricePerYear + priceSneakers + priceUniform + priceBall + priceAccessories;
+
+//Output
+
+Console.WriteLine(priceTotal);
