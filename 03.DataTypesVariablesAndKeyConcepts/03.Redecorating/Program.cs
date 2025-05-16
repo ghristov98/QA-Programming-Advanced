411f6f5bea7d1b8d52a370a2e813a746cd7a08f8
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 10:02:52 +0300

    Redecorating
    Input
    
    The input is read from the console and contains exactly 4 lines:
    
    1. Required amount of nylon (in sq.m.) - an integer number in the range [1... 100]
    
    2. Required amount of paint (in liters) - an integer number in the range [1... 100]
    
    3. Quantity of thinner (in liters) - integer number in the range [1... 75]
    
    4. Hours needed for the craftsmen to do the work - an integer number in the range [1... 50]
    
    Output
    
    Print out only one line on the console:
    
    · "{the sum of all costs}"

---
+++ 03.DataTypesVariablesAndKeyConcepts/03.Redecorating/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Input
+int amountOfNylon = int.Parse(Console.ReadLine());
+int amountOfPaint = int.Parse(Console.ReadLine());
+int quantityOfThinner = int.Parse(Console.ReadLine());
+int hoursNeededForWork = int.Parse(Console.ReadLine());
+
+// Calculations
+//•	Protective nylon - 1.50 BGN per square meter
+//•	Paint - 14.50 BGN per liter
+//•	Paint thinner - 5.00 BGN per liter
+
+double priceForNylon = (amountOfNylon + 2) * 1.50;
+double priceForPaint = amountOfPaint * 1.1 * 14.50;  // * 1.1 -> add 10%
+double priceForThinner = quantityOfThinner * 5;
+double priceForBags = 0.40;
+
+double priceForAllMaterials = priceForNylon + priceForPaint + priceForThinner + priceForBags;
+
+double oneHourWork = priceForAllMaterials * 0.3;    // 30%
+double priceForCraftsmen = hoursNeededForWork * oneHourWork;
+
+double finalPrice = priceForAllMaterials + priceForCraftsmen;
+
+// Output
+Console.WriteLine(finalPrice);
