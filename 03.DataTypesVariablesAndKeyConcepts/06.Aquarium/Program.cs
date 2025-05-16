c6c17aa58a29c793425a009345c5b5d92d15625a
Georgi Hristov <ghristov5208@gmail.com>
2025-05-16 10:08:18 +0300

    Aquarium
    Write a program that calculates the liters of water that are needed to fill the aquarium.
    
    Input
    
    From the console read 4 lines:
    
    1. Length in cm – an integer number in the range [10 ... 500].
    
    2. Width in cm – an integer number in the range [10 ... 300].
    
    3. Height in cm – an integer number in the range [10... 200].
    
    4. Percentage – floating point number in the range [0.000 ... 100.000].
    
    Output
    
    Print one number on the console:
    
    · the liters of water that the aquarium will collect, formatted to the second decimal place.

---
+++ 03.DataTypesVariablesAndKeyConcepts/06.Aquarium/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Input
+
+int lenght = int.Parse(Console.ReadLine());// lenght in cm
+int width = int.Parse(Console.ReadLine());// width in cm
+int height = int.Parse(Console.ReadLine());// height in cm
+double percentage = double.Parse(Console.ReadLine());
+
+//Calculations
+
+double volumeAquarium = lenght * width * height; //cm^3
+double volumeLiters = volumeAquarium / 1000; //liters
+double occupiedSpace = percentage / 100;
+double requiredLiters = volumeLiters * ( 1 - occupiedSpace );//liters
+
+//Output
+
+Console.WriteLine($"{requiredLiters:F2}");
