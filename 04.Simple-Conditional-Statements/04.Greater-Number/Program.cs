2ffefa4013723be284254bb3ec550ee1b9b523e1
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 12:36:57 +0300

    Greater Number
    Write a program, that:
    
    · Reads two integer numbers
    
    · Finds the greater number
    
    · Prints "Greater number: {greater number value}"

---
+++ 04.Simple-Conditional-Statements/04.Greater-Number/Program.cs
@@ -0,0 +1,15 @@
+﻿// Input
+
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+
+// Calculations
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Greater number:" + firstNumber);
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("Greater number:" + secondNumber);
+}
