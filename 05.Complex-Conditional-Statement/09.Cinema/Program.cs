ad33e42a1601b3d9d5b4916be0131e759f92d5ba
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:29:20 +0300

    Cinema
    Write a program, that calculates the price for all the tickets for a cinema movie:
    
    · Reads the type of the movie (string), the count of the rows (an integer number) and count of the seats per row (an integer number) in the cinema
    
    o Type of the movie will be one of the following: "Premiere", "Normal" and "Discount"
    
    · Prints the total price for all seats formatted to the 2nd digit after the decimal point, based on the table below:

---
+++ 05.Complex-Conditional-Statement/09.Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿// Input
+
+string type = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+
+double price1 = 7.50;
+double price2 = 12.00;
+double price3 = 5.00;
+// Output
+
+if (type == "Normal")
+{
+    int allSeatsInCinema1 = rows * seats;
+    double totalPriceforNormall = allSeatsInCinema1 * price1;
+    Console.WriteLine($"{totalPriceforNormall:F2}");
+}
+else if (type == "Premiere")
+{
+    int allSeatsInCinema2 = rows * seats;
+    double totalPriceforPremiere = allSeatsInCinema2 * price2;
+    Console.WriteLine($"{totalPriceforPremiere:F2}");
+}
+else if (type == "Discount")
+{
+    int allSeatsInCinema3 = rows * seats;
+    double totalPriceforDiscount = allSeatsInCinema3 * price3;
+    Console.WriteLine($"{totalPriceforDiscount:F2}");
+}
