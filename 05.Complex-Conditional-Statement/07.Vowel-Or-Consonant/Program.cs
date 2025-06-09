1b623d6fafd0b49251577afd3aed7fb4ffb74323
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:27:41 +0300

    Vowel or Consonant
    Write a program to check a letter for vowel or consonant:
    
    · Reads a character (letter, part of the English alphabet) from the console
    
    · Based on the value of the character, print:
    
    o If the character is vowel letter print "Vowel"
    
    Hint: Vowels letters are: A, a, E, e, I, i, O, o, U, u
    
    o If the character is consonant letter print "Consonant"

---
+++ 05.Complex-Conditional-Statement/07.Vowel-Or-Consonant/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+char letter = char.Parse(Console.ReadLine());
+
+// Output
+
+if (letter == 'A' || letter == 'a' || letter == 'E' || letter == 'e'
+    || letter == 'I' || letter == 'i' || letter == 'O' || letter == 'o'
+    || letter == 'U' || letter == 'u')
+{
+    Console.WriteLine("Vowel");
+}
+else
+{
+    Console.WriteLine("Consonant");
+}
