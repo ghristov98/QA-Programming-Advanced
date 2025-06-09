c39103f5b63fb0feb09849cf6e7d47cda5086d76
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:25:08 +0300

    Food or Drink
    Write a program that:
    
    · Reads a product (string) from the console
    
    · Based on type of the given product, print:
    
    o If product is one of following "curry", "noodles", "sushi", "spaghetti" or "bread" you have to print "food"
    
    o If product is one of following "tea", "water", "coffee" or "juice" you have to print "drink"
    
    o If the product is different from listed products above, print "unknown"

---
+++ 05.Complex-Conditional-Statement/04.Food-Or-Drink/Program.cs
@@ -0,0 +1,25 @@
+﻿// Input
+
+string product = Console.ReadLine();
+
+// Output
+
+if (product == "curry" ||
+    product == "noodles" ||
+    product == "sushi" ||
+    product == "spaghetti" ||
+    product == "bread")
+{
+    Console.WriteLine("food");
+}
+else if (product == "tea" ||
+    product == "water" ||
+    product == "coffee" ||
+    product == "juice")
+{
+    Console.WriteLine("drink");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
