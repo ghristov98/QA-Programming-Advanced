4c85d522addb5406c4c6dcfcaeef14d6cf972197
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:19:08 +0300

    Valid Triangle
    Write a program to check whether a triangle is valid, which:
    
    · Reads three integers: the sides of a triangle
    
    · Checks if each side is shorter than the sum of the other two
    
    · Prints:
    
    o "Valid Triangle" if the above condition is met
    
    o "Invalid Triangle" otherwise

---
+++ 04.Simple-Conditional-Statements/09.Valid-Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+// Output
+
+if (b + c > a && a + c > b && a + b > c)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else
+{
+    Console.WriteLine("Invalid Triangle");
+}
