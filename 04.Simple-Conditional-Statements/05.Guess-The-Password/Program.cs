1177d6874b99afb39e0db3e93fb11a73b64646b5
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 12:37:53 +0300

    Guess the Password
    Write a program for checking a password, that:
    
    · Reads a string that represents a password
    
    · Prints:
    
    o "Welcome" if the password is "s3cr3t!"
    
    o "Wrong password!" in all other cases

---
+++ 04.Simple-Conditional-Statements/05.Guess-The-Password/Program.cs
@@ -0,0 +1,14 @@
+﻿// Input
+
+string password = Console.ReadLine();
+
+// Output
+
+if (password == "s3cr3t!")
+{
+    Console.WriteLine("Welcome");
+}
+else
+{
+    Console.WriteLine("Wrong password!");
+}
