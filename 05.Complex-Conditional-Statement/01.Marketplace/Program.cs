c2dadd4643a86f503082b2b6cba2d662e4320495
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:22:18 +0300

    Marketplace
    Write a program that:
    
    · Reads two strings from the console: product (string) and day (string).
    
    · Print the price, formatted to the second digit, based on the price table:

---
+++ 05.Complex-Conditional-Statement/01.Marketplace/Program.cs
@@ -0,0 +1,42 @@
+﻿// Input
+
+using System;
+
+string product = Console.ReadLine();
+string day = Console.ReadLine();
+
+// Output
+
+if (product == "Banana")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("2.50");
+    }
+    else
+    {
+        Console.WriteLine("2.70");
+    }
+}
+else if (product == "Apple")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("1.30");
+    }
+    else
+    {
+        Console.WriteLine("1.60");
+    }
+}
+else if (product == "Kiwi")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine("2.20");
+    }
+    else
+    {
+        Console.WriteLine("3.00");
+    }
+}
