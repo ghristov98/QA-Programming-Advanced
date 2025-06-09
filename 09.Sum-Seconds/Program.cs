fc4de89604fd6215ca836b76fa4c995f6edcfc6a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 16:41:43 +0300

    Sum Seconds
    Three athletes finish in a certain number of seconds (between 1 and 50).
    
    Write a program that:
    
    · Read three integers - the athletes' times in seconds, from console
    
    · Calculate their total time in the format "minutes:seconds"

---
+++ 09.Sum-Seconds/Program.cs
@@ -0,0 +1,12 @@
+﻿// Input
+
+int timeOne = int.Parse(Console.ReadLine());
+int timeTwo = int.Parse(Console.ReadLine());
+int timeThree = int.Parse(Console.ReadLine());
+
+int totalTimeSeconds = timeOne + timeTwo + timeThree;
+
+int minutes = totalTimeSeconds / 60;
+int seconds = totalTimeSeconds % 60;
+
+Console.WriteLine($"{minutes}:{seconds:D2}");
