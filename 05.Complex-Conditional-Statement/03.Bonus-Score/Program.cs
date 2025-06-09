f85d26e950d086e10731f0c987bbae1965c74281
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:24:19 +0300

    Bonus Score
    Write a program that:
    
    · Reads points (integer number) from the console
    
    · Add bonus points to given points based on following:
    
    o If points are between 0 and 3 (inclusive), adds 5 to the given points
    
    o If points are between 4 and 6 (inclusive), adds 15 to the given points
    
    o If points are between 7 and 9 (inclusive), adds 20 to the given points
    
    · Print final points after adding the bonus points

---
+++ 05.Complex-Conditional-Statement/03.Bonus-Score/Program.cs
@@ -0,0 +1,18 @@
+﻿// Input
+
+int points = int.Parse(Console.ReadLine());
+
+// Output
+
+if (points >= 0 && points <= 3)
+{
+    Console.WriteLine(points + 5);
+}
+else if (points >= 4 && points <= 6)
+{
+    Console.WriteLine(points + 15);
+}
+else if (points >= 7 && points <= 9)
+{
+    Console.WriteLine(points + 20);
+}
