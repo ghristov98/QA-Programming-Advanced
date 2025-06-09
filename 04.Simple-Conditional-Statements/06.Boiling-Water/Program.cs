2f376a6a5785225110c555d9ce517467cc3e9914
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 12:38:43 +0300

    Boiling Water
    Write a program to check for boiling water, that:
    
    § Reads an integer number: the water temperature (in °C)
    
    § Prints:
    
    o "The water is boiling" if the number > 100
    
    o "The water is not hot enough" in all other cases

---
+++ 04.Simple-Conditional-Statements/06.Boiling-Water/Program.cs
@@ -0,0 +1,14 @@
+﻿// Input
+
+int temperature = int.Parse(Console.ReadLine());
+
+// Output
+
+if (temperature > 100)
+{
+    Console.WriteLine("The water is boiling");
+}
+else
+{
+    Console.WriteLine("The water is not hot enough");
+}
