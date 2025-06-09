f0dabbf0fd08acf6f6ed34040eabf0df2d80d4fb
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:25:53 +0300

    Invalid Number
    A given number is valid if it is in the range [100...200] or it is equals to 0.
    
    Write a program that:
    
    · Reads an integer from the console
    
    · Prints "invalid" if the entered number is NOT valid

---
+++ 05.Complex-Conditional-Statement/05.Invalid-Number/Program.cs
@@ -0,0 +1,14 @@
+﻿// Input
+
+int number = int.Parse(Console.ReadLine());
+
+// Output
+
+if (number >= 100 && number <= 200 || number == 0)
+{
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("invalid");
+}
