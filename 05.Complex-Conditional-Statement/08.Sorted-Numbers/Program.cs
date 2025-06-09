99e9751b8158662a86e14d2064dcd36208460db1
Georgi Hristov <ghristov5208@gmail.com>
2025-06-09 13:28:35 +0300

    Sorted Numbers
    Write a program, which checks for sorted 3 numbers:
    
    · Reads 3 integer numbers from the console
    
    · Based on the order of the numbers, print:
    
    o "Ascending" – if the numbers are in ascending order (from the smallest to the largest number)
    
    o "Descending" – if the numbers are in descending order (from the largest to the smallest number)
    
    o "Not sorted" – in any other case

---
+++ 05.Complex-Conditional-Statement/08.Sorted-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Input
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 =  int.Parse(Console.ReadLine());
+
+// Output
+
+if (number1 < number2 && number2 < number3)
+{
+    Console.WriteLine("Ascending");
+}
+else if (number3 < number2 && number2 < number1)
+{
+    Console.WriteLine("Descending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
