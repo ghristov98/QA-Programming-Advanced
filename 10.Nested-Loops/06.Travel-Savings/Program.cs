0e7247b1a6a27af543c17757eee1b1def90cb5aa
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 18:59:31 +0300

    Travel Savings
    Write a program that calculate the money collection for multiple travel destinations:
    
    · Read a destination (string) and needed budget (floating-point number) for the destination
    
    · Read many times amounts of collected money, until they are enough for the destination (starting from 0)
    
    o Print:
    
    "Collected: {sum}" where sum is formatted to 2nd digit
    
    or
    
    "Going to {destination}!"
    
    · Read another destination and budget and collect money again
    
    · A destination "End" ends the program

---
+++ 10.Nested-Loops/06.Travel-Savings/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string destination = Console.ReadLine();
+
+while (destination != "End")
+{
+    double neededSum = double.Parse(Console.ReadLine());
+    double savedSum = 0;
+
+    while (savedSum < neededSum)
+    {
+        double currentSum = double.Parse(Console.ReadLine());
+        savedSum += currentSum;
+        Console.WriteLine($"Collected: {savedSum:F2}");
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+
+    destination = Console.ReadLine();
+
+}
