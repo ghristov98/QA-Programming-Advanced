a193388ca14f134748c87393ed76556bf6723a2a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 09:11:12 +0300

    Number Operations
    Write a program to evaluate operations, which:
    
    · Reads 2 real numbers and math operator (string) from the console
    
    · Possible given values for the math operator are: "+", "-", "*", "/"
    
    · Apply the operator with given numbers
    
    · Print output in the following format, where result is formatted to the second digit:
    
    "{N1} {operator} {N2} = {result}"

---
+++ 06.Conditional-Statements/04.Number-Operations/Program.cs
@@ -0,0 +1,28 @@
+﻿// Input
+
+double n1 = double.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine());
+string mathOpr = Console.ReadLine();
+
+// Output
+
+if (mathOpr == "+")
+{
+    double resultSum = n1 + n2;
+    Console.WriteLine($"{n1} {mathOpr} {n2} = {resultSum:F2}");
+}
+else if (mathOpr == "-")
+{
+    double resultSub = n1 - n2;
+    Console.WriteLine($"{n1} {mathOpr} {n2} = {resultSub:F2}");
+}
+else if (mathOpr == "*")
+{
+    double resultMulti = n1 * n2;
+    Console.WriteLine($"{n1} {mathOpr} {n2} = {resultMulti:F2}");
+}
+else if (mathOpr == "/")
+{
+    double resultDevi = n1 / n2;
+    Console.WriteLine($"{n1} {mathOpr} {n2} = {resultDevi:F2}");
+}
