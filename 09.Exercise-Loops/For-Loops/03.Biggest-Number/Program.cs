d89667f97cfcb6aca4ffa73154a807ab03832e42
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:32:42 +0300

    Biggest Number
    Write a program to find the biggest among given n numbers:
    
    · Read an integer number n (the amount of input numbers) and n integer numbers from the console
    
    · Find and print the biggest number

---
+++ 09.Exercise-Loops/For-Loops/03.Biggest-Number/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int countNumbers = int.Parse(Console.ReadLine());
+
+int maxNumbers = int.MinValue; // най голямо число
+
+
+for (int count = 1; count <= countNumbers; count++)
+{
+    // за всяко едно число от първото до последното
+    //1. въвеждаме стойността на числото
+    int number = int.Parse(Console.ReadLine());
+
+    //2. проверка дали текущото число е най - голямо
+    if (number > maxNumbers)
+    {
+        maxNumbers = number;
+    }
+}
+Console.WriteLine(maxNumbers);
