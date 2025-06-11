65c0b02481bf59cb5e23d0e52adc0fb41d41ed46
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 09:12:18 +0300

    Vacation Expenses
    Write a program, which calculates vacation expenses:
    
    · From the console read: season (string), accommodation type (string) and count of the days (integer)
    
    o Season will be one of the following: "Spring", "Summer", "Autumn" and "Winter"
    
    o Accommodation type will be one of the following: "Hotel" and "Camping
    
    · Based on the table below, you have to calculate expenses for the vacation with the given accommodation type, season and count of the days.
    
    · For the calculation: use price per night (extracted from the table below) multiplied by count of the days.
    
    · Print the total expenses, formatted to the second digit after the decimal point

---
+++ 06.Conditional-Statements/05.Vacation-Expenses/Program.cs
@@ -0,0 +1,63 @@
+﻿// Input
+
+string season = Console.ReadLine();
+string accom = Console.ReadLine();
+int daysLeft = int.Parse(Console.ReadLine());
+
+double pricePerNight = 0;
+// Output
+
+switch (season)
+{
+    case "Spring":
+        if (accom == "Hotel") 
+        {
+            // 30 - 20% = 24
+            pricePerNight = 24;
+        }
+        else if (accom == "Camping") 
+        {
+            // 10 - 20% = 8
+            pricePerNight = 8;
+        }
+    break;
+    case "Summer":
+        if (accom == "Hotel")
+        {
+            // 50 - 0% = 50
+            pricePerNight = 50;
+        }
+        else if (accom == "Camping")
+        {
+            // 30 - 0% = 30
+            pricePerNight = 30;
+        }
+        break;
+    case "Autumn":
+        if (accom == "Hotel")
+        {
+            // 20 - 30% = 14
+            pricePerNight = 14;
+        }
+        else if (accom == "Camping")
+        {
+            // 15 - 30% = 10.5
+            pricePerNight = 10.50;
+        }
+        break;
+    case "Winter":
+        if (accom == "Hotel")
+        {
+            // 40 - 10% = 36
+            pricePerNight = 36;
+        }
+        else if (accom == "Camping")
+        {
+            // 10 - 10% = 9
+            pricePerNight = 9;
+        }
+        break;
+}
+
+double totalSum = daysLeft * pricePerNight;
+Console.WriteLine($"{totalSum:F2}");
