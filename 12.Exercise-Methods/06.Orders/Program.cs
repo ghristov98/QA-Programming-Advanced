38eae0d6bf93e6aa7a4172393eab27bbe4be2bbc
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:19:51 +0300

    Orders
    Write a program that:
    
    · Reads a string on the first line from the console, representing a product:
    
    "coffee", "water", "coke" or "snacks"
    
    · Reads an integer on the second line from the console, representing the quantity of the product
    
    · Create a method that calculates and prints the total price of an order
    
    · The method should receive two parameters: product and quantity
    
    · The prices for a single item of each product are:
    
    coffee – 1.50
    
     water – 1.00
    
     coke – 1.40
    
     snacks – 2.00
    
    · Print the result, formatted to the second digit

---
+++ 12.Exercise-Methods/06.Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string product = Console.ReadLine();
+int quantity =  int.Parse(Console.ReadLine());
+
+PrintFinalPrice(product, quantity);
+
+static void PrintFinalPrice(string product, int quantity)
+{
+    double productPrice = 0;
+
+    switch (product)
+    {
+        case "coffee":
+              productPrice = 1.50;
+            break;
+        case "water":
+              productPrice = 1.00;
+            break;
+        case "coke":
+              productPrice = 1.40;
+            break;
+        case "snacks":
+              productPrice = 2.00;
+            break;
+    }
+
+    double finalPrice = productPrice * quantity;
+    Console.WriteLine($"{finalPrice:F2}");
+
+}
