41f338508d4a470107576e66cb3acd8a37bccf56
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:15:52 +0300

    Multiply Evens by Odds
    Write a program that multiplies the sum of all even digits of a number by the sum of all odd digits of the same number:
    
    · Read an integer number from the console
    
    · Create a method called GetMultipleOfEvenAndOdds()
    
    · Create a method GetSumOfEvenDigits()
    
    · Create GetSumOfOddDigits()
    
    · You may need to use Math.Abs() for negative numbers

---
+++ 12.Exercise-Methods/05.Multiply-Even-By-Ods/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+static int GetSomeOfEvenDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        if (lastDigit % 2 == 0)
+        {
+            sum += lastDigit;
+        }
+        num /= 10;
+    }
+    return sum;
+}
+
+static int GetSumOfOddDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int lastDigit = num % 10;
+        if (lastDigit % 2 != 0)
+        {
+            sum += lastDigit;
+        }
+        num /= 10;
+    }
+    return sum; 
+}
+static int GetMultiplyOfEvenAndOdds(int num)
+{
+    return GetSomeOfEvenDigits(num) * GetSumOfOddDigits(num);
+}
+
+Console.WriteLine(GetMultiplyOfEvenAndOdds(num));
