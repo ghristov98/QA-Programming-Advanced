53c33999fed1079ffff0c294c99ae4960f576d2a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:15:21 +0300

    Password Validator
    Write a program that checks if a given password is valid.
    
    The password validation rules are:
    
    · It should contain 6 – 10 characters (inclusive)
    
    · It should contain only letters and digits
    
    · It should contain at least 2 digits
    
    If it is not valid, for any unfulfilled rule print the corresponding message:
    
    · "Password must be between 6 and 10 characters"
    
    · "Password must consist only of letters and digits"
    
    · "Password must have at least 2 digits"

---
+++ 12.Exercise-Methods/04.Password-Validator/Program.cs
@@ -0,0 +1,58 @@
+﻿
+        static bool IsValidLength(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    
+
+
+static bool IsValidContent(string password)
+{
+    foreach (char symbol in password)
+    {
+        if (!char.IsLetterOrDigit(symbol))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool IsValidCountDigits(string password)
+{
+    int count = 0;
+    foreach (char symbol in password)
+    {
+        if (char.IsDigit(symbol))
+            count++;
+    }
+    return count >= 2;
+}
+string password = Console.ReadLine();
+if (IsValidLength(password) && IsValidContent(password) && IsValidCountDigits(password))
+{
+    Console.WriteLine("Password is valid");
+}
+else
+{
+    if (!IsValidLength(password))
+    {
+        Console.WriteLine("Password must be between 6 and 10 characters");
+    }
+    if (!IsValidContent(password))
+    {
+        Console.WriteLine("Password must consist only of letters and digits");
+    }
+    if (!IsValidCountDigits(password))
+    {
+        Console.WriteLine("Password must have at least 2 digits");
+    }
+}
