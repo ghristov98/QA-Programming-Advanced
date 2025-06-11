72ea6dfbfc614a394f28f788d003cc74ae8a7530
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:00:06 +0300

    Sum of Digits Calculator
    Write a program that:
    
    · Continuously read integers until "End" is entered from the console
    
    o Print the sum of digits for each integer, use the following format:
    
    "Sum of digits = {sum}"
    
    · Finally, print "Goodbye"

---
+++ 10.Nested-Loops/07.Sum-Of-Digits-Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    int number = int.Parse(input);
+
+    int sum = 0;
+
+    while (number > 0)
+    {
+        int lastDigit = number % 10; // take the last digit
+        sum += lastDigit;
+        number /= 10; // remove the last digit
+    }
+
+    Console.WriteLine($"Sum of digits = {sum}" );
+
+    input = Console.ReadLine();
+}
+Console.WriteLine("Goodbye");
