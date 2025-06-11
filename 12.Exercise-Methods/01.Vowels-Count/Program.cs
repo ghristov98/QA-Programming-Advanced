ab79a07aad5faded26024237561f0b45edc0d8af
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:10:13 +0300

    Vowels Count
    Write a program that:
    
    · Read a text (string) from the console
    
    · Create a method that receives a text
    
    · Find the count of the vowels contained in the text
    
    · Print the count of the vowels in the text

---
+++ 12.Exercise-Methods/01.Vowels-Count/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string text = Console.ReadLine();
+int countVowels = GetVowelsCount(text);
+Console.WriteLine(countVowels);
+    static  int GetVowelsCount(string text)
+    {
+        int count = 0; // броя на гласните букви
+        // обхождаме всеки един символ в текста
+        for (int position = 0; position <= text.Length - 1; position++) 
+        {
+            char  currentSymbol = text[position];
+            // проверка дали символа е гласна буква
+            if (currentSymbol == 'A' || currentSymbol == 'a'
+                || currentSymbol == 'E' || currentSymbol == 'e'
+                || currentSymbol == 'O' || currentSymbol == 'o'
+                || currentSymbol == 'I' || currentSymbol == 'i'
+                || currentSymbol == 'U' || currentSymbol == 'u')
+            {
+                count ++; // увеличаваме броя на гласните букви с 1
+            }
+;
+        }
+        // знаем броя на гласните букви -> count
+        return count;
+}
