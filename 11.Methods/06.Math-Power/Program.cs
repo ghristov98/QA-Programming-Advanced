f70b81fa4c32276eca2d07ef467db81b61e4a8c1
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:07:44 +0300

    Math Power
    Write a program that:
    
    · Reads two integer numbers from the console: base number and power
    
    · Create a method, which receives two numbers as parameters:
    
    o The first number – the base
    
    o The second number – the power
    
    · The method should return the base raised to the given power

---
+++ 11.Methods/06.Math-Power/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int power = int.Parse(Console.ReadLine());
+
+        double sum = Math.Pow(num, power);
+        Console.WriteLine(sum);
+        
+    }
+    
+}
