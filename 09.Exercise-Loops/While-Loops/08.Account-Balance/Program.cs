2778878a59b0e2480e27a69fc02554e524111cbd
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:37:47 +0300

    Account Balance
    Write a program to calculate an account balance:
    
    · Read a sequence of incomes / expenses, until "End" is read
    
    · Add the money to the balance (starting form 0)
    
    · Print "Increase: {money}" or "Decrease: {money}", where value is formatted to the second decimal digit
    
    · Finally, print the account balance, formatted to the second decimal digit in the following format: "Balance: {account balance}"

---
+++ 09.Exercise-Loops/While-Loops/08.Account-Balance/Program.cs
@@ -0,0 +1,13 @@
+﻿double balance = 0.0;
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "End") break;
+    double amount = double.Parse(input);
+    balance += amount;
+    if (amount >= 0)
+        Console.WriteLine($"Increase: {amount:F2}", amount);
+    else
+        Console.WriteLine($"Decrease: {Math.Abs(amount):F2}");
+}
+Console.WriteLine($"Balance: {balance:F2}");
