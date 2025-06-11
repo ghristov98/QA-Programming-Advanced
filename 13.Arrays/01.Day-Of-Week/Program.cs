a2033a9f62632086aa17b35d442ef8d0f89b6932
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:21:48 +0300

    Day of Week
    Write a program that:
    
    · Reads an integer number from the console
    
    · If the number is in range [1…7] print:
    
    o "Monday" for number 1
    
    o "Tuesday" for number 2
    
    o "Wednesday" for number 3
    
    o "Thursday" for number 4
    
    o "Friday" for number 5
    
    o "Saturday" for number 6
    
    o "Sunday" for number 7
    
    · If the number is out of the given range print "Invalid day!"

---
+++ 13.Arrays/01.Day-Of-Week/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int index = int.Parse(Console.ReadLine());
+
+string[] daysOfWeek = new string[] 
+{ 
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+if (index < 1 || index > 7)
+{
+    Console.WriteLine("Invalid day!");
+}
+else
+{
+    Console.WriteLine(daysOfWeek[index-1]);
+}
+
