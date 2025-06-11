d09b8e7054a12cf2f525710f787dca9ccbbe3c26
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:14:50 +0300

    Multiplication Sign
    Write a program that:
    
    · Reads three integer numbers (num1, num2 and num3) from the console
    
    · Finds if num1 * num2 * num3 (the product) is negative, positive or zero
    
    · Print:
    
    o negative - if the product is smaller than 0
    
    o positive - if the product is bigger than 0
    
    o zero - if the product is equals to 0

---
+++ 12.Exercise-Methods/03.Multiplication-Sign/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+PrintResultType(firstNumber, secondNumber, thirdNumber);
+
+static void PrintResultType(int n1, int n2, int n3)
+{
+    int result = n1 * n2 * n3;
+    if (result < 0)
+    {
+        Console.WriteLine("negative");
+    }
+    else if (result > 0)
+    {
+        Console.WriteLine("positive");
+    }
+    else if (result == 0)
+    {
+        Console.WriteLine("zero");
+    }
+}
