c61ea16387e1e782a30709a8d59da7098a6095d3
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 09:22:18 +0300

    First N Numbers Sum
    Write a program, which sums the numbers 1…n:
    
    · Reads an integer number n from the console
    
    · Sums all numbers from 1 to n
    
    · Prints the numbers and the sum on the console as shown below

---
+++ 07.For-Loop/02.First-N-Numbers-Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= n; i++)
+{
+     
+    Console.Write(i);
+    sum += i;
+    if (i < n)
+    {
+        Console.Write('+');
+    }
+}
+Console.WriteLine($"={sum}");
