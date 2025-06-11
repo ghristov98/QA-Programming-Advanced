3da23e8e5f6e8db5de1c16615e8f2b002115c23b
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:04:53 +0300

    Grades
    Write a program that:
    
    · Reads a grade (floating-point number) from the console
    
    · Create a method that receives a grade between 2.00 and 6.00
    
    · Prints the corresponding grade in words
    
    o "Fail" - If the grade is in range 2.00 – 2.99 (inclusively)
    
    o "Average" - If the grade is in range 3.00 – 3.49 (inclusively)
    
    o "Good" - If the grade is in range 3.50 – 4.49 (inclusively)
    
    o "Very good" - If the grade is in range 4.50 – 5.49 (inclusively)
    
    o "Excellent" - If the grade is in range 5.50 – 6.00 (inclusively)

---
+++ 11.Methods/02.Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double inputGrade = double.Parse(Console.ReadLine());
+
+        PrintGradesInWords(inputGrade);
+    }
+
+
+    static void PrintGradesInWords(double grade)
+    {
+        if (grade >= 2.00 && grade <= 2.99)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (grade >= 3.00 && grade <= 3.49)
+        {
+            Console.WriteLine("Average");
+        }
+        else if (grade >= 3.50 && grade <= 4.49)
+        {
+            Console.WriteLine("Good");
+        }
+        else if (grade >= 4.50 && grade <= 5.49)
+        {
+            Console.WriteLine("Very good");
+        }
+        else if (grade >= 5.50 && grade <= 6)
+        {
+            Console.WriteLine("Excellent");
+        }
+    }
+
+}
