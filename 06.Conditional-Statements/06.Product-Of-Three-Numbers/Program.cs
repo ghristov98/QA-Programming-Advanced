be49a0b2b3ba0e8bcdf0195481c17ac2238886ff
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 09:13:13 +0300

    Product of Three Numbers
    Write a program that calculates the sign of the product of three numbers:
    
    · Reads 3 real numbers from the console
    
    · Print the sign of the product of the three given numbers: "positive", "negative" or "zero"

---
+++ 06.Conditional-Statements/06.Product-Of-Three-Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿// Input
+
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber  = double.Parse(Console.ReadLine());
+double thirdNumber = double.Parse(Console.ReadLine());
+
+int countNegativeNumbers = 0;
+
+// Output
+
+if (firstNumber == 0 || secondNumber == 0 || thirdNumber == 0)
+{
+    Console.WriteLine("zero");
+}
+else
+{
+    if (firstNumber < 0)
+    {
+        countNegativeNumbers++;
+    }
+    if (secondNumber < 0)
+    {
+        countNegativeNumbers++;
+    }
+    if (thirdNumber < 0)
+    {
+        countNegativeNumbers++;
+    }
+    if (countNegativeNumbers % 2 == 0)
+    {
+        Console.WriteLine("positive");
+    }
+    else
+    {
+        Console.WriteLine("negative");
+    }
+}
