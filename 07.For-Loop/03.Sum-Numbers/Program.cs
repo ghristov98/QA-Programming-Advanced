d065d62a488710e8c4719d2f37b49f91297a31a0
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 09:23:17 +0300

    Sum Numbers
    Write a program to sum given N numbers:
    
    · Read integer number n – the count of numbers to sum
    
    · Read n floating-point numbers and print their sum

---
+++ 07.For-Loop/03.Sum-Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int num = int.Parse(Console.ReadLine());   
+
+double sum = 0;
+for (int i = 1; i <= num; i++)
+{
+    double currentNumber = double.Parse(Console.ReadLine());
+
+    sum += currentNumber;
+
+}
+
+Console.WriteLine(sum);
