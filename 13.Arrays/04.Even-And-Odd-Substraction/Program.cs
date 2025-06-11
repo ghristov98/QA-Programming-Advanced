15842a2794cb344535f523bb56ac741f9cdc35d1
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:23:37 +0300

    Even and Odd Subtraction
    Write a program that:
    
    · Read an array of integers (from a single line separated with a space)
    
    · Calculates the difference between the sum of the even and the sum of the odd numbers in an array
    
    · Print the difference

---
+++ 13.Arrays/04.Even-And-Odd-Substraction/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int evenSum = 0;
+int oddSum = 0;
+
+foreach (int item in numbers)
+{
+    if (item % 2 == 0) // проверяваме дали стойността е четна
+    {
+        evenSum += item;
+    }
+    else               // проверяваме дали стойността е нечетна
+    {
+        oddSum += item;
+    }
+}
+
+Console.WriteLine(evenSum - oddSum);
