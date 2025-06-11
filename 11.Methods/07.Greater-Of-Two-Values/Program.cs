84f06de9e05c8ee7368c5ab9aded14a4d937a0fc
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:08:29 +0300

    Greater of Two Values
    Write a program that:
    
    · Reads a type (string) and two values of this type from the console
    
    · Entered type can be one of the following values: "int", "char" or "string"
    
    · Create methods which can compare int, char or string
    
    · Return the biggest of the two values

---
+++ 11.Methods/07.Greater-Of-Two-Values/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string type = Console.ReadLine();
+        string firstValue  = Console.ReadLine();
+        string secondValue  = Console.ReadLine();
+
+        CompateTwoValues(type, firstValue, secondValue);
+    }
+
+    private static string CompateTwoValues(string type, string? firstValue, string? secondValue)
+    {
+        if (type == "int")
+        {
+            int a = int.Parse(firstValue);
+            int b = int.Parse(secondValue);
+
+            Console.WriteLine(CompateTwoIntegers(a, b));
+        }
+        else if (type == "char")
+        {
+            char a = char.Parse(firstValue);
+            char b = char.Parse(secondValue);
+
+            Console.WriteLine(CompareTwoChars(a, b));
+        }
+        else if (type == "string")
+        {
+         Console.WriteLine(CompareTwoStrings(firstValue, secondValue));
+        }
+
+        return "";
+    }
+
+    private static string CompareTwoStrings(string? firstString, string? secondString)
+    {
+        int result = firstString.CompareTo(secondString);
+
+        if (result > 1)
+        {
+            return firstString;
+        }
+        else
+        {
+            return secondString;
+        }
+    }
+
+    private static char CompareTwoChars(char a, char b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    private static int CompateTwoIntegers(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
