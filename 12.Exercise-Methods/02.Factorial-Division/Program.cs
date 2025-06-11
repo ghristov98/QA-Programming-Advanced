c7618cd7c113d7e026d4fe59d58c39ca2a4325d8
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:14:15 +0300

    Factorial Division
    Write a program that:
    
    · Read two integers numbers from the console in range [1…10] · Calculate the factorial of each number
    
    · Divide the first calculated factorial by the second calculated factorial (integer division)
    
    · Print the result of the division

---
+++ 12.Exercise-Methods/02.Factorial-Division/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+//изчисляваме факториел на първото число
+int factFirstNum = CalculateFactorial(firstNum);
+
+//изчисляваме факториел на второто число
+int factSecondNum = CalculateFactorial(secondNum);
+
+//разделяме факториел на първото число на факториел на второто число
+int result = factFirstNum / factSecondNum;
+
+Console.WriteLine(result);
+static int CalculateFactorial(int number)
+{
+    int fact = 1;//факториела на числото
+	for (int i = 1; i <= number; i++)
+	{
+		//i -> съхранявам всяко едно число от 1 до моето
+		fact = fact * i;
+	}
+	return fact;
+}
