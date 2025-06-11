6c5f19d19079571fec109ce1be5c6bc157dbf284
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:01:00 +0300

    Prime Numbers
    Write a program that:
    
    · Reads two integer numbers: start of the range and end of the range
    
    · Print all prime numbers in given range

---
+++ 10.Nested-Loops/08.Prime-Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int startRange = int.Parse(Console.ReadLine());
+int endRange  = int.Parse(Console.ReadLine());
+
+for (int currNum = startRange; currNum <= endRange; currNum++)
+{
+    int counter = 0;
+    bool isPrime = true;
+    for (int j = 1; j <= currNum; j++) // serch prime number
+    {
+        if (currNum % j == 0)
+        {
+            counter++;
+
+        }
+    }
+     if (counter > 2)
+    {
+        isPrime = false;
+    }
+
+     if (isPrime)
+           Console.Write(currNum + " ");
+}
