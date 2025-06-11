e4c5cae22870b49faca0baf94eb8f2e06903245a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 11:24:38 +0300

    Latin Letters
    Write a program to print the Latin letters in certain range:
    
    · Read two letters (chars), each on separate line
    
    · Print all letters in the specified range inclusively

---
+++ 07.For-Loop/07.Latin-Letters/Program.cs
@@ -0,0 +1,8 @@
+﻿
+char firstLetter = char.Parse(Console.ReadLine());
+char secondLetter = char.Parse(Console.ReadLine());
+
+for (int i = firstLetter; i <= secondLetter; i++)
+{
+    Console.Write($"{(char)i} ");
+}
