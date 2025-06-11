e5cb661cc30b7848130b7fa746eaecc58f96f97a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 17:12:34 +0300

    Triangle of Stars
    Write a program to print a triangle of stars like shown in the examples:
    
    · Read the size (integer number) of a triangle from the console
    
    · Print a triangle of stars

---
+++ 10.Nested-Loops/03.Triangle-Of-Stars/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int c = int.Parse(Console.ReadLine());
+
+for (int a = 1; a <= c; a++)
+{
+    for (int b = 1; b <= a; b++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine();
+}
