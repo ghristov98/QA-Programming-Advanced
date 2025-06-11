00462c7159937e353f6ea1eb69474656bd5a44c2
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:05:38 +0300

    Printing Triangle
    Write a program that:
    
    · Reads an integer number N from the console
    
    · Create a method for printing triangle depending on value of the number N

---
+++ 11.Methods/03.Printing-Traingles/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        PrintFirstPartTriangle(n);
+        PrintSecondPartTriangle(n);
+    }
+
+    private static void PrintFirstPartTriangle(int side)
+    {
+        for (int row = 1; row <= side; row++) // how many rows to print
+        {
+            for (int i = 1; i <= row; i++) // how many numbers in a row to print
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static void PrintSecondPartTriangle(int side)
+    {
+        for (int row = side - 1; row >= 1; row--) // how many rows to print 
+        {
+            for (int i = 1; i <= row; i++) // how many number in a row
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
