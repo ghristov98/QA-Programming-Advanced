ac33f4d41ba102f59806d61d984ec0fd8e9282d2
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:32:01 +0300

    Multiplication Table
    Write a program that prints a multiplication table:
    
    · Reads an integer number n from the console
    
    · Print a multiplication table of size 10 for given integer n in the following format:
    
    "{n} x {i} = {result}" for each i in the range [1…10]

---
+++ 09.Exercise-Loops/For-Loops/02.Multiplication-Table/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+int n = int.Parse(Console.ReadLine());
+
+
+for (int number = 1; number <= 10; number++)
+{
+    int result = n * number;
+    Console.WriteLine($"{n} x {number} = {result}");
+}
