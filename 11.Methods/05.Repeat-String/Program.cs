6a7c85dded87f5b0a33c79131c4d23d0b5a38dd0
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:07:01 +0300

    Repeat String
    Write a program that:
    
    · Reads a text (string) and repeat count (integer number) from the console
    
    · Write a method that receives a string and a repeat count
    
    · The method should return a new string, containing the initial one, repeated count times without space

---
+++ 11.Methods/05.Repeat-String/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        int num = int.Parse(Console.ReadLine());
+
+        ReturningText(text, num);
+    }
+
+    private static void ReturningText(string text, int num)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.Write(text);
+        }
+
+    }
+
+}
