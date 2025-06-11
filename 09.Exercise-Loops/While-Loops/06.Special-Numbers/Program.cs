e0508699e9ec581057720ef4a57060d7c9f4196d
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:35:32 +0300

    Special Number
    Write a program to check if given number is special:
    
    · Special numbers are divisible by all of their digits without remainder
    
    · Reads an integer number and check whether it is a special number
    
    · If the number IS special print: "{num} is special"
    
    · If the number is NOT special print: "{num} is not special"

---
+++ 09.Exercise-Loops/While-Loops/06.Special-Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿//входни данни
+int number = int.Parse(Console.ReadLine()); //число, което ще обработвам и ще му взимам цифрите
+int startNumber = number; //първоначалната стойност на числото, която няма да променям
+
+bool isSpecial = true;
+//isSpecial = true -> числото е специално
+//isSpecial = false -> числото не е специално
+
+//взимаме цифрите на числото
+//number > 0 -> има цифри в него -> продължаваме да ги взимаме
+//number <= 0 -> няма цифри в него -> прекратяваме взимане
+while (number > 0)
+{
+    //взимаме последната цифра от числото
+    int lastDigit = number % 10;
+
+    //проверка дали моето число се дели на тази цифра
+    if (startNumber % lastDigit != 0)
+    {
+        //числото не се дели на някоя от цифрите му -> числото не е специално
+        isSpecial = false;
+        break;
+    }
+
+    //премахваме последната цифра от числото
+    number = number / 10;
+}
+
+
+
+//отпечатване
+if (isSpecial)
+{
+    //числото е специално
+    Console.WriteLine(startNumber + " is special");
+}
+else
+{
+    //isSpecial == false
+    //числото не е специално
+    Console.WriteLine(startNumber + " is not special");
+}
