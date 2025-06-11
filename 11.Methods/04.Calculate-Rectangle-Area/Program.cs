724cb29078464ad383a87e904b62faab6a389d64
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 19:06:19 +0300

    Calculate Rectangle Area
    Write a program that:
    
    · Reads two integer numbers from the console: width and length
    
    · Create a method which returns rectangle area with given width and length
    
    Hint: Rectangle area can be calculated when you multiply width and length of the rectangle.

---
+++ 11.Methods/04.Calculate-Rectangle-Area/Program.cs
@@ -0,0 +1,18 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+         int width = int.Parse(Console.ReadLine());
+         int length = int.Parse(Console.ReadLine());
+
+
+        int area = CalculateRengtangeArea(width, length);
+        Console.WriteLine(area);
+    }
+
+    private static int CalculateRengtangeArea(int width, int length)
+    {
+        return width * length;
+    }
+}
