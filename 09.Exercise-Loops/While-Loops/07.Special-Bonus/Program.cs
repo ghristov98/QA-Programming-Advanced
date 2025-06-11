ddaa398b5d4dd90001a5a4864c08de6c96dd7ecf
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:36:39 +0300

    Special Bonus
    Write a program to apply a 20% bonus for certain number:
    
    · Reads an integer number from the console: the "stop number"
    
    · Keep reading integers until it finds the stop number
    
    · When the stop number is found, increase the value of the previous number before it with 20% and print it

---
+++ 09.Exercise-Loops/While-Loops/07.Special-Bonus/Program.cs
@@ -0,0 +1,22 @@
+﻿int stopNumber = int.Parse(Console.ReadLine());
+//ако от конзолата се въведе число = stopNumber -> прекратяваме въвеждането на числа
+
+//while цикъл
+//повтаряме: въвеждаме цяло число
+//стоп: въведеното число == stopNumber
+//продължаваме: въведено число != stopNumber
+
+int previousNumber = 0;
+int enteredNumber = int.Parse(Console.ReadLine());
+
+while (enteredNumber != stopNumber)
+{
+    previousNumber = enteredNumber;
+    enteredNumber = int.Parse(Console.ReadLine());
+}
+
+//enteredNumber == stopNumber
+//1. намеря предишното въведено
+//2. предишно въведено + 20%
+//3. отпечатвам
+Console.WriteLine(previousNumber * 1.20);
