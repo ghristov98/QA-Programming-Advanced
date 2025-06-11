2f1dc2ff272a04c47444787dd153a9fc7a0a4211
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:31:16 +0300

    Power of Number
    Write a program to calculate number raising to a certain power:
    
    · Reads an integer number n from the console
    
    · Reads an integer number p which represents power from the console
    
    · Print the result of n to the power of p

---
+++ 09.Exercise-Loops/For-Loops/01.Power-Of-Number/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = int.Parse(Console.ReadLine());
+int pow = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int count = 1; count <= pow; count++)
+{
+    result = result * num;
+}
+Console.WriteLine(result);
