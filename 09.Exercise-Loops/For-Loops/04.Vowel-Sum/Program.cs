a2921a89b01f515f3cd2158eaf955def795a402a
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 16:33:22 +0300

    Vowel Sum
    Write a program to sum N vowels, according to the table below:
    
    · Read an integer number N: the count of characters
    
    · Read N characters and for each vowel character adds its value from the table to the resul

---
+++ 09.Exercise-Loops/For-Loops/04.Vowel-Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int countCharacters  = int.Parse(Console.ReadLine());
+
+int sum = 0;//сума от точките на символите
+for (int count = 1; count <= countCharacters; count++)
+{
+    char symbol = char.Parse(Console.ReadLine());
+
+    //проверка дали е: а, e, i, o, u
+    if (symbol == 'a')
+    {
+        sum += 1;
+    }
+    else if (symbol == 'e')
+    {
+        sum += 2;
+    }
+    else if (symbol == 'i')
+    {
+        sum += 3;
+    }
+    else if (symbol == 'o')
+    {
+        sum += 4;
+    }
+    else if (symbol == 'u')
+    {
+        sum += 5;
+    }
+}
+Console.WriteLine(sum);
