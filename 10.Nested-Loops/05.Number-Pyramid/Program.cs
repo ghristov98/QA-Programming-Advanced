055b68272509cd03d1b81cb7e81692acd5173e91
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 17:16:39 +0300

    Number Pyramid
    Write a program that:
    
    · Reads an integer number n from the console
    
    · Prints a pyramid of numbers as shown in the examples

---
+++ 10.Nested-Loops/05.Number-Pyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int counter = 0;
+
+for (int rows = 1;  rows <= n;  rows++)
+{
+	for (int col = 1; col <= rows; col++)
+	{
+		counter++;
+		Console.Write($"{counter} ");
+		if (counter == n)
+			break;
+	}
+	if (counter == n)
+		break;
+	Console.WriteLine();
+}
