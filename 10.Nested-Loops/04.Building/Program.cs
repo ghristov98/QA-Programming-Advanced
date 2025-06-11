fee6bdab1f0522a7bb290fd33d702851e40c87ca
Georgi Hristov <ghristov5208@gmail.com>
2025-06-11 17:15:02 +0300

    Building
    Write a program to print a table, representing a building:
    
    · Reads two integer numbers from the console: floors count and estates count per floor
    
    · Identifiers consist of: {type}{floor}{number}, e.g. L65, A12, O24
    
    · Odd floors hold apartments (type A), e.g. A10, A11, A12, …
    
    · Even floors hold offices (type O), e.g. O20, O21, O22, …
    
    · The last floor holds large apartments (type L), e.g. L60, L61, L62

---
+++ 10.Nested-Loops/04.Building/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int countFloors = int.Parse(Console.ReadLine());
+int countEstates = int.Parse(Console.ReadLine());
+
+for (int floors = countFloors; floors >= 1; floors--)
+{
+	for (int apts = 0; apts < countEstates; apts++)
+	{
+		if (countFloors == floors)
+		{
+            Console.Write($"L{floors}{apts} ");
+		}
+		else if (floors % 2 == 0)
+		{
+			Console.Write($"O{floors}{apts} ");
+		}
+		else
+		{
+            Console.Write($"A{floors}{apts} ");
+		}
+	}
+    Console.WriteLine();
+}
