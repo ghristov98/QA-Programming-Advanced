5ed7a1e19c8910d31be89798db29c98f458f141b
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:38:07 +0300

    7. Top Integers
    Write a program that:
    
    · Reads sequence of integer numbers from the first line of the console
    
    · Find all the top integers in a sequence
    
    · Top integer is an integer that is bigger than all the elements to its right

---
+++ 14.Exercise-Arrays/07.Top-Integers/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] inputArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    int currElement = inputArray[i];
+    bool isBigger = true;
+    //започвам от следващия десен елемент
+    for (int j = i + 1; j < inputArray.Length; j++)
+    {
+        int nextRightElement = inputArray[j];
+
+        if (currElement <= nextRightElement)
+        {
+            isBigger = false;
+            break;
+        }
+    }
+   if (isBigger)
+    {
+        Console.Write(currElement + " ");
+    }
+}
