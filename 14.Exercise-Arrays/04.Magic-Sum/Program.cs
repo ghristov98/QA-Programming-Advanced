633ebc1fb6c38dc820664ebd87141c39a821d075
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:35:50 +0300

    4. Magic Sum
    Write a program that:
    
    · Read a sequence of integer numbers from the first line of the console
    
    · Read an integer number (control number) from the second line of the console
    
    · Print all unique pairs in an array of integers whose sum is equal to a control number

---
+++ 14.Exercise-Arrays/04.Magic-Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] inputArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+int controlNum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < inputArray.Length - 1; i++) // минавам през всички елементи без последния
+{
+    int leftElement = inputArray[i];
+
+    for (int j = i + 1; j < inputArray.Length; j++) // минавам прес всички елементи надясно от текущия
+    {
+        int rightElement = inputArray[j];
+
+        int sum = leftElement + rightElement;
+
+        if (sum == controlNum)
+        {
+          Console.WriteLine($"{leftElement} {rightElement}");
+            break;
+        }
+    }
+
+    
+    
+}
