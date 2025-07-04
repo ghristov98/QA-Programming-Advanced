0fcee13e49796278ed8e9ef170e650821084fc04
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:37:29 +0300

    6. Array Rotation
    Write a program that:
    
    · Reads sequence of integer numbers from the first line of the console
    
    · Reads an integer number (rotations you have to perform) from the second line of the console
    
    · Each rotation is when the first element goes at the end
    
    · Print the resulting sequence

---
+++ 14.Exercise-Arrays/06.Array-Rotation/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] inputArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+int rotaitons = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotaitons; i++)
+{
+    // запазвам си първия елемент
+    int firstElement = inputArray[0];
+
+    // местя всички елементи от индекс 1, една позиция на ляво  
+    for (int j = 1; j < inputArray.Length; j++)
+    {
+        inputArray[j - 1] = inputArray[j];
+    }
+    //местя първият елемент като последен
+    inputArray[inputArray.Length - 1] = firstElement;
+}
+Console.WriteLine(string.Join(" ", inputArray));
