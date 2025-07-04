774d395b364ec22c03d69475cf20801efb5e725d
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 08:20:25 +0300

    Add files via upload

--- 18.Unit-Testing-Arrays/TestApp/Average.cs
+++ 18.Unit-Testing-Arrays/TestApp/Average.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TestApp;
+
+public class Average
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
 
