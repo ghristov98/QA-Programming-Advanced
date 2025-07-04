f089a2d8f48a9a7823dcb24c3bf2bc39da1625d5
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:36:29 +0300

    5. Zig-Zag Arrays
    Write a program that:
    
    · Creates two empty integer arrays
    
    · Reads an integer number N from the console
    
    · Reads two integer numbers on the next N lines
    
    · Form two integer arrays as shown below

---
+++ 14.Exercise-Arrays/05.Zig-Zag-Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+for (int  i = 0;  i < n;  i++)
+{
+
+    int[] rowData = Console.ReadLine()
+                           .Split()
+                           .Select(int.Parse)
+                           .ToArray();
+
+    if (i % 2 == 0) // четна итерация
+    {
+        firstArray[i] = rowData[0];
+        secondArray[i] = rowData[1];
+    }
+    else // нечетна итерация    
+    {
+        firstArray[i] = rowData[1];
+        secondArray[i] = rowData[0];
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
