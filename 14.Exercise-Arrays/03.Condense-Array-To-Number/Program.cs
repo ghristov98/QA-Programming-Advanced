ce9abfcb2895a4ec6a107c2ce8e82ba7d8075206
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:35:05 +0300

    3. Condense Array to Number
    Write a program that:
    
    · Read a sequence of integer numbers from the console
    
    · Condense them by summing adjacent couples of elements until a single integer is obtained

---
+++ 14.Exercise-Arrays/03.Condense-Array-To-Number/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] firstArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+int counter = firstArray.Length;
+
+if (firstArray.Length == 1 )
+    Console.WriteLine(firstArray[0]);
+
+while (counter > 1)
+{
+    int[] condensedArray = new int[counter - 1];
+
+    for (int i = 0; i < counter - 1; i++)
+    {
+        condensedArray[i] = firstArray[i] + firstArray[i + 1];
+    }
+
+    counter--;
+
+    firstArray = condensedArray;
+
+    if (counter == 1)
+    {
+        Console.WriteLine(condensedArray[0]);
+      
+    }
+}
