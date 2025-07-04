57fc7d9091167ed7ef92a76a8e88dd27c682661a
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 08:28:56 +0300

    Add files via upload

--- 20.Exercise-Unit-Testing-Methods/TestApp/Email.cs
+++ 20.Exercise-Unit-Testing-Methods/TestApp/Email.cs
@@ -1,1 +1,16 @@
+﻿using System.Net.Mail;
 
+namespace TestApp;
+
+public class Email
+{
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return false;
+        }
+
+        return MailAddress.TryCreate(email, out _);
+    }
+}
---
+++ 20.Exercise-Unit-Testing-Methods/TestApp/MiddleCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp;
+
+public class MiddleCharacters
+{
+    public static string GetMiddleChars(string input)
+    {        
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return "Empty string";
+        }
+        
+        int length = input.Length;
+
+        if(length % 2 == 0)
+        {
+            return input.Substring((length / 2) - 1, 2);
+        }
+        else
+        {
+            return input.Substring(length / 2, 1);
+        }
+    }
+}
