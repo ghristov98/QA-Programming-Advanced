296b851b7bc3c6f8b003284594caf85d03253549
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:40:43 +0300

    2. List of Products
    Write a program that:
    
    · Read an integer number n and n lines of products
    
    · Print a numbered list of all the products ordered by name

---
+++ 15.Lists/02.List-Of-Products/Program.cs
@@ -0,0 +1,22 @@
+﻿int countProducts = int.Parse(Console.ReadLine());
+List<string> products = new List<string>(); 
+
+
+for (int product = 1; product <= countProducts; product++)
+{
+    
+    string productName = Console.ReadLine();
+    products.Add(productName);
+}
+
+
+
+products.Sort(); 
+
+
+int number = 1; 
+foreach (string product in products)
+{
+    Console.WriteLine(number + "." + product);
+    number++;
+}
