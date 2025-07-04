b1b3ba72f26bb2b8b07d6653cf3e23300de083d7
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:55:27 +0300

    2. House Party
    Create a program that keeps track of the guests that are going to a house party.
    
    On the first line, of input you are going to receive the number of commands that will follow.
    
    On the next lines, you are going to receive some of the following:
    
    "{name} is going!"
    
    · You have to add the person, if they are not on the guestlist already.
    
    · If the person is on the list print the following to the console: "{name} is already in the list!"
    
    "{name} is not going!"
    
    · You have to remove the person, if they are on the list.
    
    · If not, print out: "{name} is not in the list!"
    
    Finally, print all of the guests, each on a new line.

---
+++ 16.Exercise-Lists/02.House-Party/Program.cs
@@ -0,0 +1,40 @@
+﻿
+List<string> guesList = new List<string>();
+
+int countOfCommands = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < countOfCommands; i++)
+{
+    string[] commandArguments = Console.ReadLine().Split(" ");
+
+    string name = commandArguments[0];
+    string goingOrNot = commandArguments[2];
+
+    if (goingOrNot == "going!") // ще добавям някого в списъка
+    {
+        if (guesList.Contains(name)) // проверяваме дали го има в списъка
+        {
+            Console.WriteLine($"{name} is already in the list!");
+        }
+        else
+        {
+            guesList.Add(name);
+        }
+    }
+    else if (goingOrNot == "not") // ще премахвам някой от списъка
+    {
+        if (guesList.Contains(name))  // проверяваме дали го има в списъка
+        {
+            guesList.Remove(name);
+        }
+        else
+        {
+            Console.WriteLine($"{name} is not in the list!");
+        }
+    }
+}
+
+foreach (var guestName in guesList)
+{
+    Console.WriteLine(guestName);
+}
