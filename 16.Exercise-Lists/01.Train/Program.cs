d2f5a8017e7d64cce144fbec7caa9cb283984aba
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:43:24 +0300

    1. Train
    On the first line, we will receive a list of wagons (integers). Each integer represents the number of passengers that are currently in each wagon of the passenger train. On the next line, you will receive the max capacity of a wagon, represented as a single integer. Until you receive the "end" command, you will be receiving two types of input:
    
    · Add {passengers} – add a wagon to the end of the train with the given number of passengers.
    
    · {passengers} – find a single wagon to fit all the incoming passengers (starting from the first wagon).
    
    In the end, print the final state of the train (all the wagons separated by a space)

---
+++ 16.Exercise-Lists/01.Train/Program.cs
@@ -0,0 +1,39 @@
+﻿
+List<int> train = Console.ReadLine()
+                         .Split(" ")
+                         .Select(int.Parse)
+                         .ToList();
+
+int wagonCapacity = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    string[] cmdArg = command.Split(" ");
+
+    if (cmdArg[0] == "Add")
+    {
+        int passangersToAdd = int.Parse(cmdArg[1]);
+
+        train.Add(passangersToAdd);
+    }
+    else
+    {
+        int passangersToAdd = int.Parse(cmdArg[0]);
+
+        for (int i = 0; i < train.Count; i++)
+        {
+            int currentPassangers = train[i];
+
+            if (wagonCapacity - currentPassangers >= passangersToAdd)
+            {
+                train[i] = currentPassangers + passangersToAdd;
+                break;
+            }
+        }
+        Console.WriteLine(command);
+    }
+}
+
+Console.WriteLine(string.Join(" ", train));
