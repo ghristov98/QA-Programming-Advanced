16352e2d70b67aae78e3e201d7ee53fc9f573795
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 08:03:47 +0300

    4. List Manipulation Basics
    Write a program that:
    
    · Reads a list of integers
    
    · Then until you receive "end", you will receive different commands:
    
    o "Add {number}": add a number to the end of the list
    
    o "Remove {number}": remove a number from the list
    
    o "RemoveAt {index}": remove a number at a given index
    
    o "Insert {number} {index}": insert a number at a given index
    
    · When you receive the "end" command, print the final state of the list (separated by spaces)
    
    Note: All the indices will be valid!

---
+++ 16.Exercise-Lists/04.List-Manipulation-Basics/Program.cs
@@ -0,0 +1,36 @@
+﻿List<int> numbers = Console.ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToList();
+
+string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string[] cmdArg = input.Split(" ");
+    string command = cmdArg[0]; 
+
+    if (command == "Add")
+    {
+        int number = int.Parse(cmdArg[1]);
+        numbers.Add(number);
+    }
+    else if (command == "Remove")
+    {
+        int number = int.Parse(cmdArg[1]);
+        numbers.Remove(number);
+    }
+    else if (command == "RemoveAt")
+    {
+        int index = int.Parse(cmdArg[1]);
+        numbers.RemoveAt(index);
+    }
+    else if (command == "Insert")
+    {
+        int number = int.Parse(cmdArg[1]);
+        int index = int.Parse(cmdArg[2]);
+        numbers.Insert(index, number);
+    }
+    input = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
