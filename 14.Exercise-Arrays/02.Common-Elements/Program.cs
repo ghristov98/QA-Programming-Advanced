7590dbd94414befd81502c7737cab984c0356c33
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:34:14 +0300

    2. Common Elements
    Write a program that:
    
    · Read two integer arrays with the same length from the console
    
    · Prints common elements in two arrays, space-separated

---
+++ 14.Exercise-Arrays/02.Common-Elements/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] firstArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+int[] secondArray = Console.ReadLine()
+                           .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+
+
+for (int a = 0; a < firstArray.Length; a++)
+{
+    for (int b = 0; b < secondArray.Length; b++)
+    {
+        if (firstArray[a] == secondArray[b])
+        {
+            Console.Write(firstArray[a] + " ");
+            break;
+        }
+    }
+}
