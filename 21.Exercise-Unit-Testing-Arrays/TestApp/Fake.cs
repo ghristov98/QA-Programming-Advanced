579ae41b945e80cf68ff783176dc6ea0e0416432
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 08:32:37 +0300

    Add files via upload

--- 21.Exercise-Unit-Testing-Arrays/TestApp/Fake.cs
+++ 21.Exercise-Unit-Testing-Arrays/TestApp/Fake.cs
@@ -1,1 +1,12 @@
+﻿using System;
+using System.Linq;
 
+namespace TestApp;
+
+public class Fake
+{
+    public static char[] RemoveStringNumbers(char[]? arr)
+    {
+        return arr.Where(c => !char.IsDigit(c)).ToArray();
+    }
+}
