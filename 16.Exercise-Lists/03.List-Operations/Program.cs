898ea26509a069219db6495cb1ecec809331c868
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:56:08 +0300

    3. List Operations
    The first input line will hold a list of integers. Until we receive the "End" command, we will be given operations we have to apply to the list.
    
    The possible commands are:
    
    · Add {number} – add the given number to the end of the list
    
    · Insert {number} {index} – insert the number at the given index
    
    · Remove {index} – remove the number at the given index
    
    · Shift left {count} – first number becomes last. This has to be repeated the specified number of times
    
    · Shift right {count} – last number becomes first. To be repeated the specified number of times
    
    Note: the index given may be outside of the bounds of the array. In that case print: "Invalid index".

---
+++ 16.Exercise-Lists/03.List-Operations/Program.cs
@@ -0,0 +1,75 @@
+﻿
+List<int> numbers = Console.ReadLine()
+                           .Split(" ")
+                           .Select(int.Parse)
+                           .ToList();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] cmdArg = input.Split(" ");
+    string command = cmdArg[0];
+
+    if (command == "Add")
+    {
+        int number = int.Parse(cmdArg[1]);
+
+        numbers.Add(number);
+    }
+    else if (command == "Insert")
+    {
+        int number = int.Parse(cmdArg[1]);
+        int index = int.Parse(cmdArg[2]);
+
+        if (index >= 0 && index < numbers.Count)
+        {
+            numbers.Insert(index, number);
+        }
+        else
+        {
+            Console.WriteLine("Invalid index");
+
+        }
+    }
+    else if (command == "Remove")
+    {
+        int index = int.Parse(cmdArg[1]);
+
+        if (index >= 0 && index < numbers.Count)
+        {
+            numbers.RemoveAt(index);
+        }
+        else
+        {
+            Console.WriteLine("Invalid index");
+        }
+
+    }
+    else if (command == "Shift")
+    {
+        string leftOrRight = cmdArg[1];
+        int count = int.Parse(cmdArg[2]);
+        if (leftOrRight == "left")
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int firstElement = numbers[0]; // взимаме първия елемент
+                numbers.Add(firstElement); // добавяме го последен
+                numbers.RemoveAt(0); // премахваме първия елемент
+            }
+        }
+        else if (leftOrRight == "right")
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int lastElement = numbers[numbers.Count - 1]; // взимаме последния елемент
+                numbers.Insert(0, lastElement); // вкарваме го като първи елемент
+                numbers.RemoveAt(numbers.Count - 1);    // премахваме последния елемент
+            }
+        }
+    }
+    input = Console.ReadLine();
+
+}
+Console.WriteLine(string.Join(" ", numbers));
