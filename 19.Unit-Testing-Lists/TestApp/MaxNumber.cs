c25e5775a0fbaad19bebf4da667f1423825b6161
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 08:25:14 +0300

    Add files via upload

--- 19.Unit-Testing-Lists/TestApp/MaxNumber.cs
+++ 19.Unit-Testing-Lists/TestApp/MaxNumber.cs
@@ -1,1 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace TestApp;
+
+public class MaxNumber
+{
+    public static int FindMax(List<int> numbers)
+    {
+        return numbers.Max();
+    }
+}
