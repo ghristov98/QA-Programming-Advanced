d8fe0edf3a0ecd42453807e12ba5a68ada60e0a3
Georgi Hristov <ghristov5208@gmail.com>
2025-07-04 07:33:03 +0300

    1. Equal Arrays
    Write a program that:
    
    · Read two arrays with the same length from the console
    
    · Check whether they are identical or not
    
    o If the arrays are identical, print: "Arrays are identical."
    
    o If the arrays are NOT identical, print: "Arrays are not identical."
    
    Note: Arrays are identical if their elements are equal.

---
+++ 14.Exercise-Arrays/01.Equal-Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] firstArray = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+int[] secondArray = Console.ReadLine()
+                           .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+bool isIdentical = true;
+
+for (int x = 0; x < firstArray.Length; x++)
+{
+        if (firstArray[x] != secondArray[x])
+        {
+            isIdentical = false;
+            break;
+        }
+}
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
